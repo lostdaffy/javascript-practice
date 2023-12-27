eb18ff7e731823cb81a1af32a541cfe30b5a0a5d
Satyaprakash <84344984+SatyaaSingh@users.noreply.github.com>
2023-12-27 09:23:40 +0000

    hmm

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,32 @@
+// let score = 33
+// let score = "33abc"
+// let score = null
+// let score = false
+// let score = true
+// let score = "lostdaffy"
+
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score)
+
+
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+
+
+
+// "33" => 33
+// "33abc" => NaN (Not a Number) but this type is number
+// true => 1 , false => 0
+
+
+let isLoggedIn = 1;
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+console.log(booleanIsLoggedIn)
+
+// 1 => true, 0 => false, "" => false, "lostdaffy" => true 
