cc3b13a23c47a0539d6ba3b4188ad22411f1ce2f
Satyaprakash <84344984+SatyaaSingh@users.noreply.github.com>
2023-12-27 09:26:58 +0000

    hg

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -30,3 +30,10 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 console.log(booleanIsLoggedIn)
 
 // 1 => true, 0 => false, "" => false, "lostdaffy" => true 
+
+let someNumber = 44;
+
+let stringNumber = String(someNumber);
+
+console.log(stringNumber);
+console.log(typeof stringNumber);
