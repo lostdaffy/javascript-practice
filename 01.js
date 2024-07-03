93e0aadd14502d384dfdfe7151db2695b416e733
Satyaprakash <satyaasingh001@gmail.com>
2024-07-03 18:44:26 +0000

    day 1

---
+++ 01.js
@@ -0,0 +1,6 @@
+// Print Hello lostdaffy
+
+console.log("Hello lostdaffy")
+console.log(`hello lostdaffy`)
+console.log('Hello lostdaffy')
+
---
+++ 02.js
@@ -0,0 +1,5 @@
+// Print the number from 1 to 5 to the console using a loop
+
+for (let i = 1; i <= 5; i++) {
+    console.log(i);
+}
